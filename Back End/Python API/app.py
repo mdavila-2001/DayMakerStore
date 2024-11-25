9a591ffe59158fb25b6c24261dd782390abc528f
mdavila-2001 <dmarcelo201@gmail.com>
2024-11-24 20:13:54 -0400

    Changes added

--- Back End/Python API/app.py
+++ Back End/Python API/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS  # Importar CORS
 from config import Config
 from models import db
 from apis.usuarios import api as usuarios_ns
@@ -10,21 +11,22 @@ from apis.tipo_usuarios import api as tipo_usuario_ns
 
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app)  # Habilitar CORS
+
 db.init_app(app)
 
 api = Api(
     app,
     version='1.0',
     title='Prueba de API Backend',
-    description='API REST para gestionar productos de videojuegos',
-    doc='/docs'  # URL donde se mostrará Swagger UI
+    description='API REST para gestionar productos, usuarios y pedidos',
 )
 
 api.add_namespace(usuarios_ns)
-api.add_namespace(tipo_usuario_ns)
 api.add_namespace(productos_ns)
 api.add_namespace(pedidos_ns)
 api.add_namespace(detalles_pedido_ns)
+api.add_namespace(tipo_usuario_ns)
 
 if __name__ == '__main__':
     app.run(debug=True)
